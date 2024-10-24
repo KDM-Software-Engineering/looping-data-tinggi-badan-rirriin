573a0a40f47dcbc0e13c7c9dbcdb2c8731059cb4
rirriin <meilian237650@student.smkn1kandeman.sch.id>
2024-10-24 14:12:03 +0700

    selesai

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let tinggiBadan = [150, 155, 148, 165, 170, 168, 166]
 let namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
 
 for (let  i = 0; i < tinggiBadan.length; i++) {
-  console.log (`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiBadan[i]}cm ` );
+  console.log (`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiBadan[i]}cm`);
 }
 }
 runner ()
